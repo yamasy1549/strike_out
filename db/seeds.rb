93fad3d1652e0bc1f29c28bd5492359a5396ee18
yamasy1549 <sanae@yamasy.info>
2015-10-18 01:21:08 +0900

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.create(email: 'hoge@example.com', password: 'password')
+
 10.times do |i|
   Result.create(name: Gimei.kanji, score: rand(0..60), adult: true)
   Result.create(name: Gimei.first.hiragana, score: rand(0..60), adult: false)
