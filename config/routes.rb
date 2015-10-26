cddef92c957c40eda6d1b838e5b5ee71bf085eff
yamasy1549 <sanae@yamasy.info>
2015-10-26 21:22:01 +0900

    Change root to results#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'results#index'
+  root to: 'results#new'
 
   devise_for :users, only: [:sign_in, :sign_out, :session]
   resources :results, except: [:show]
