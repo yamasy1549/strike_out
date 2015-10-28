91c70d16bb719abf29872e866ff74dd0afe0fd4d
yamasy1549 <sanae@yamasy.info>
2015-10-28 21:16:16 +0900

    Auto reload results#index

---
+++ app/assets/stylesheets/reload.js
@@ -0,0 +1,4 @@
+var meta = document.createElement('meta');
+meta.setAttribute('http-equiv', 'refresh');
+meta.setAttribute('content', '30');
+document.getElementsByTagName('head')[0].appendChild(meta);
